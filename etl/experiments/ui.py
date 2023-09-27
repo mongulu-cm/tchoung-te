a223269e3a433d4eef770a80826ddd01a9b14b88
anekameni <Kamenialexnea@gmail.com>
2023-09-28 00:06:17 +0200

    upt: finetune promt to avoid some answer

--- etl/experiments/ui.py
+++ etl/experiments/ui.py
@@ -12,11 +12,11 @@ from langchain.prompts.chat import (
 )
 import chainlit as cl
 
-system_template = """Vous êtes un assistant AI  qui fournit des informations sur les associations camerounaises en France.
-Vous recevez une question et fournissez une réponse claire et structurée.Lorsque cela est pertinent, utilisez des points et des listes pour structurer vos réponses.
+system_template = """Vous êtes un assistant IA qui fournit des informations sur les associations camerounaises en France. Vous recevez une question et fournissez une réponse claire et structurée. Lorsque cela est pertinent, utilisez des points et des listes pour structurer vos réponses.
 
-Utilisez les éléments de contexte suivants pour répondre à la question de l'utilisateur.
-Si vous ne connaissez pas la réponse, dites simplement que vous ne savez pas, n'essayez pas d'inventer une réponse.
+Utilisez les éléments de contexte suivants pour répondre à la question de l'utilisateur. Si vous ne connaissez pas la réponse, dites simplement que vous ne savez pas, n'essayez pas d'inventer une réponse.
+
+Si vous souhaitez connaître le nombre d'associations, je vous recommande de visiter le site web "tchoung-te.mongulu.cm" pour obtenir des informations actualisées à ce sujet.
 ----------------
 {context}"""
 messages = [
@@ -25,11 +25,15 @@ messages = [
 ]
 CHAT_PROMPT = ChatPromptTemplate.from_messages(messages)
 
-
-loader = CSVLoader(file_path="ref-rna-real-mars-2022-enriched-qualified.csv", encoding="utf-8")
-data = loader.load()
+embedding_pth = "embeddings"
 embeddings = OpenAIEmbeddings()
-vectors = FAISS.from_documents(data, embeddings)
+if os.path.exists(embedding_pth):
+    vectors = FAISS.load_local(embedding_pth, embeddings)
+else:
+    loader = CSVLoader(file_path="ref-rna-real-mars-2022-enriched-qualified.csv", encoding="utf-8")
+    data = loader.load()
+    vectors = FAISS.from_documents(data, embeddings)
+    vectors.save_local(embedding_pth)
 
 llm = ChatOpenAI(max_tokens=500,temperature=0,model_name="gpt-3.5-turbo")
 chain_type_kwargs = {"prompt": CHAT_PROMPT}
